4e1739d99f55143ae4ec433c4a8ba9e48f01007e
Ruben Veerman <rubjeveerman@gmail.com>
2017-05-29 13:02:07 +0200

    doet ie et?

---
+++ CScherpProject/Controllers/SongsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CScherpProject.Controllers
+{
+    public class SongsController : Controller
+    {
+        // GET: Songs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
